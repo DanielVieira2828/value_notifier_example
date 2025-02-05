1dbb296a97686c01df24add040cf4adc0732b2f4
DanielVieiraSS <danielvieira2828@gmail.com>
2025-02-05 08:05:02 -0300

    folder organization

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:change_notifier_state/presentation/controllers/theme_controller.dart';
+import 'package:change_notifier_state/presentation/pages/my_home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -23,41 +24,5 @@ class MyApp extends StatelessWidget {
             home: const MyHomePage(title: 'Flutter Demo Home Page'),
           );
         });
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("ChangeNotifier"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Trocar tema do app"),
-            ListenableBuilder(
-                listenable: themeController,
-                builder: (contex, child) {
-                  return Switch(
-                      value: themeController.isDarkTheme,
-                      onChanged: (value) {
-                        themeController.changeTheme();
-                      });
-                })
-          ],
-        ),
-      ),
-    );
   }
 }
---
+++ lib/presentation/pages/my_home_page.dart
@@ -0,0 +1,38 @@
+import 'package:change_notifier_state/main.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ChangeNotifier"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Trocar tema do app"),
+            ListenableBuilder(
+                listenable: themeController,
+                builder: (contex, child) {
+                  return Switch(
+                      value: themeController.isDarkTheme,
+                      onChanged: (value) {
+                        themeController.changeTheme();
+                      });
+                })
+          ],
+        ),
+      ),
+    );
+  }
+}
